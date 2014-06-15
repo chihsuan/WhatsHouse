e13f2251df63d20cb97eb846ba383590f7b2b8aa
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-06-15 15:59:43 +0800

    fixed a bug in sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,23 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
-  def create
-	@user = User.new(params[:user])    # Not the final implementation!
+ def new
+	@user = User.new
+ end
+ def create
+    @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the WhatsHouse!"
-    	redirect_to @user
+      redirect_to @user
     else
-     	render 'new'
-    end	
-  end 
+      render 'new'
+    end
+  end
 
-  private:
+  private
 
-  def user_params
+    def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
+
 end
