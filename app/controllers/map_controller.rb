95bac6d1bd714256b2b7644af8640bcb593f4410
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-06-16 01:40:48 +0800

    fixed a syntax error in porduction mode occuur

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class MapController < ApplicationController
-	def index:
+	def index
 		@real_price_list = RealPriceDeal.all
 		gon.real_price_list = @real_price_list.as_json()
 	end
