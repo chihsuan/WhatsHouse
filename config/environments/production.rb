ced29fe78ded7dffbfa59df57ff094522298d87d
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-06-18 18:47:48 +0800

    change eamil setting to gmail service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,20 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+ 
+  #email setting
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"	
+  config.action_mailer.smtp_settings = {
+  		address: "smtp.gmail.com",
+  		port: 587,
+  		domain: "thawing-waters-8314.herokuapp.com",
+  		authentication: "html",
+  		enable_starttls_auto: true,
+  		user_name: ENV["GMAIL_USERNAME"],
+  		password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
