6e1b0a6a9002bf916bb020c025bafca95c0479b7
brandboat <brandboat@gmail.com>
2014-07-02 17:32:50 +0800

    parse imageURL

--- app/assets/javascripts/rent_houses.js
+++ app/assets/javascripts/rent_houses.js
@@ -1,11 +1,10 @@
 // image slider show
-//
-    jssor_slider1_starter = function (containerId) {
-        var options = {
-            $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$,
-                $ChanceToShow: 2
-            }
-        };
-        var jssor_slider1 = new $JssorSlider$(containerId, options);
+jssor_slider1_starter = function (containerId) {
+    var options = {
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$,
+            $ChanceToShow: 2
+        },
     };
+    var jssor_slider1 = new $JssorSlider$(containerId, options);
+};
