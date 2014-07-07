afe98a701e8c2da8f01a2187b064cb6da3c492d5
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-07 15:50:59 +0800

    update css style and add image

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -15,6 +15,15 @@ class ContentController < ApplicationController
 		@rent_houses = RentHouse.select("id, lat, lng, address, data")
 		gon.real_price_list = @rent_houses.as_json()
 	end
+	
+	def about
+		
+	end
+
+	def contact
+
+	end
+
 =begin
 	def buy
 		@sale_houses = SaleHouse.select("id, lat, lng, address, data")
