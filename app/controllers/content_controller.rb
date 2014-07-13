39d9b789be371ce0f0ac80436ae4970e95cb47a4
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-13 13:11:10 +0800

    add comment and delete some useless code

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,30 +1,29 @@
 class ContentController < ApplicationController
-	
+	# main content controller
+
 	def analysis
 		@real_price_list = RealPriceDeal.select("id, lat, lng, location, data")
 		gon.real_price_list = @real_price_list.as_json()
 	end
 
+	# home page
 	def content
 		if signed_in? 
 			redirect_to rent_path
 		end
 		@user = User.new
-		#@sale_houses = SaleHouse.select("id, lat, lng, address, data")
-		#gon.real_price_list = @sale_houses.as_json()
 	end
 
+	# rent page
 	def rent
 		@rent_houses = RentHouse.select("id, lat, lng, address, data")
 		gon.real_price_list = @rent_houses.as_json()
 	end
 	
 	def about
-		
 	end
 
 	def contact
-
 	end
 
 =begin
