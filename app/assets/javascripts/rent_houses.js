f4d4074f982a1dd3601de5988273064afcec2c92
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-22 00:01:05 +0800

    bug fixed

--- app/assets/javascripts/rent_houses.js
+++ app/assets/javascripts/rent_houses.js
@@ -1,44 +1,43 @@
-	//= require jquery.easing.min
-	//= require jquery.address
-	// image slider show
+//= require jquery.easing.min
+//= require jquery.address
+// image slider show
 
-	var rent_page = function () {
+var rent_page = function () {
 
 
-			// add input field by jQuery
-			var max_input = 5;
-			var inputs_wrapper = $('.img_input_wrapper');
-			var add_button = $('.add_more_button');
+	// add input field by jQuery
+	var max_input = 5;
+	var inputs_wrapper = $('.img_input_wrapper');
+	var add_button = $('.add_more_button');
 
-			var x = inputs_wrapper.length;
-			var fieldCount = 1;
+	var x = inputs_wrapper.length;
+	var fieldCount = 1;
 
-			$(add_button).click( function(e) {
-				
-				if ( x <= max_input ){
-					fieldCount++;
-					
-					$(inputs_wrapper).append('<div><div class="ui left labeled icon input">' +
-											 '<input id="rent_house_img" name="img[]" type="text" >' +
-													'<i class="photo icon"></i>' +
-													'<div class="ui corner label">' +
-														'<i class="icon asterisk"></i> </div>'+ 
-											'<a href="#" class="removeclass">×</a></div>'
-					);
-					x++;
-					console.log(fieldCount);
-				}
-				return false;
-			});
+	$(add_button).click( function(e) {
 
-			$('body').on('click', '.removeclass', function(e){
+		if ( x <= max_input ){
+			fieldCount++;
 
-					if( x > 1 ){
-						$(this).parent('div').remove();
-						x--;
-					}
-				return false;
-			})
-	}
+			$(inputs_wrapper).append('<div><div class="ui left labeled icon input">' +
+				'<input id="rent_house_img" name="img[]" type="text" >' +
+				'<i class="photo icon"></i>' +
+				'<div class="ui corner label">' +
+				'<i class="icon asterisk"></i> </div>'+ 
+				'<a href="#" class="removeclass">×</a></div>'
+				);
+			x++;
+		}
+		return false;
+	});
 
-	$(document).ready(rent_page)
+	$('body').on('click', '.removeclass', function(e){
+
+		if( x > 1 ){
+			$(this).parent('div').remove();
+			x--;
+		}
+		return false;
+	})
+}
+
+$(document).ready(rent_page)
