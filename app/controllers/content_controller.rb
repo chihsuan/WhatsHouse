a3fdcd2493842e4ea8ae463ad7ff7f557f56c3e0
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-21 20:52:53 +0800

    remove useless code

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,11 +1,6 @@
 class ContentController < ApplicationController
 	# main content controller
 
-	def analysis
-		@real_price_list = RealPriceDeal.select("id, lat, lng, location, data")
-		gon.real_price_list = @real_price_list.as_json()
-	end
-
 	# home page
 	def content
 		if signed_in? 
@@ -26,10 +21,4 @@ class ContentController < ApplicationController
 	def contact
 	end
 
-=begin
-	def buy
-		@sale_houses = SaleHouse.select("id, lat, lng, address, data")
-		gon.real_price_list = @sale_houses.as_json()
-	end
-=end
 end
