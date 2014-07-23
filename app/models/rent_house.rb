e9205a9d98fb2c7fcaf90da3b0fb909419c13e2d
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-23 13:22:24 +0800

    add validates in model

--- app/models/rent_house.rb
+++ app/models/rent_house.rb
@@ -1,8 +1,10 @@
 class RentHouse < ActiveRecord::Base
 	belongs_to :user, :counter_cache => true
 	validates :user_id, presence: true
-	validates_presence_of :use, :structure, :address, :price, :owner, :tel, :information, :img
-	validates :information, :length => { :minimum => 1, :maximum => 120 }
+	validates_presence_of :use, :structure, :address, :price, :owner, :tel, :information, :people
+	validates_length_of :information, :minimum => 1, :maximum => 200
+	validates_numericality_of :price, :people
+
 	geocoded_by :address,
 		:latitude => :lat, :longitude => :lng
 	after_validation :geocode
@@ -17,10 +19,10 @@ class RentHouse < ActiveRecord::Base
 		@lng_upper = self.lng + 0.005
 		@lng_lower = self.lng - 0.005
 		@number_list =  ""
-		@number_list += Stores.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
-		@number_list += Market.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
-		@number_list += Dining.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
-		@number_list += Station.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s 
+		@number_list += Stores.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).count.to_s + " "
+		@number_list += Market.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).count.to_s + " "
+		@number_list += Dining.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).count.to_s + " "
+		@number_list += Station.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).count.to_s 
 		#@number_list += Hospitals.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
 		#@number_list += Childcareroster.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
 		#@number_list += PoliceStation.where(:lat => @lat_lower...@lat_upper, :lng => @lng_lower...@lng_upper).length.to_s + " "
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,37 @@
 class User < ActiveRecord::Base
- 	has_many :sale_houses, dependent: :destroy
- 	has_many :rent_houses, dependent: :destroy
- 	has_many :authorizations, dependent: :destroy
+	has_many :sale_houses, dependent: :destroy
+	has_many :rent_houses, dependent: :destroy
+	has_many :authorizations, dependent: :destroy
 
-  	before_save { self.email = email.downcase }
-  	before_create :create_remember_token
-  	before_create { generate_token(:auth_token) }
-  	
-  	validates :name, presence: true, length: { maximum: 50 }
-  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  	validates :password, confirmation: true
-  	validates :password_confirmation, presence: true
-  	has_secure_password
-  		validates :password, length: { minimum: 6 }
-	
+	before_save { self.email = email.downcase }
+	before_create :create_remember_token
+	before_create { generate_token(:auth_token) }
+	after_save :send_comfirm_mail
+
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence:   true,
+		format:     { with: VALID_EMAIL_REGEX },
+		uniqueness: { case_sensitive: false }
+	validates :password, confirmation: true
+	validates :password_confirmation, presence: true
+	validates :password, length: { minimum: 6 }
+	has_secure_password
+
 	def User.new_remember_token
-    	SecureRandom.urlsafe_base64
-  	end
+		SecureRandom.urlsafe_base64
+	end
 
-  	def User.hash(token)
-    	Digest::SHA1.hexdigest(token.to_s)
-  	end
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
 
 	def send_password_reset
-  		generate_token(:password_reset_token)
-  		self.password_reset_sent_at = Time.zone.now
-  		self.password = SecureRandom.urlsafe_base64
-  		save!
-  		UserMailer.password_reset(self).deliver
+		generate_token(:password_reset_token)
+		self.password_reset_sent_at = Time.zone.now
+		self.password = SecureRandom.urlsafe_base64
+		save!
+		UserMailer.password_reset(self).deliver
 	end
 
 	def add_provider(auth_hash)
@@ -40,15 +41,19 @@ class User < ActiveRecord::Base
 		end
 	end
 
-  private
+	private
 
-    def create_remember_token
-      self.remember_token = User.hash(User.new_remember_token)
-    end
+	def create_remember_token
+		self.remember_token = User.hash(User.new_remember_token)
+	end
 
 	def generate_token(column)
-  		begin
-    	self[column] = SecureRandom.urlsafe_base64
-  		end while User.exists?(column => self[column])
+		begin
+			self[column] = SecureRandom.urlsafe_base64
+		end while User.exists?(column => self[column])
+	end
+	
+	def send_comfirm_mail
+		UserMailer.confirm(:email).deliver
 	end
 end
