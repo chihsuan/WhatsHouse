07b62dd1f5fd2a0d7d69c443d0e1f437a0a0feee
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-26 00:53:12 +0800

    add edit update testing

--- spec/controllers/rent_houses_controller_spec.rb
+++ spec/controllers/rent_houses_controller_spec.rb
@@ -6,6 +6,7 @@ describe RentHousesController do
     @user = User.new
     @rent_house = RentHouse.new
     @rent_house.stub!(to_param: '1')
+    @user.stub!(to_param: '1')
     controller.stub!(:authenticate_user!)
     controller.stub!(:current_user).and_return(@user)
   end
@@ -34,20 +35,32 @@ describe RentHousesController do
       current_user.rent_houses.should_receive(:build).with("use"=>"living", "address"=>"tainan, taiwan", "price"=> '1000', "owner"=>"chihsuan", "structure"=>"4 room", "information"=>"for rent", "tel"=>"02-229222", "img" => "123.jpg 111.jpg", "people" => "4").and_return(@rent_house)
       @rent_house.should_receive(:save).and_return(true)
       post :create, user_id: 1, id: 1, rent_house: { use: 'living', structure: '4 room', 
-                           address: 'tainan, taiwan', price: 1000, 
-                           owner:  'chihsuan', tel: '02-229222',
-                           information:  'for rent', people: 4, img: '123.jpg 111.jpg'}
-      
+        address: 'tainan, taiwan', price: 1000, 
+        owner:  'chihsuan', tel: '02-229222',
+        information:  'for rent', people: 4, img: '123.jpg 111.jpg'}
+
       response.should be_redirect     
     end
   end
 
   describe 'GET edit' do
+    it 'should render successful' do
+      current_user.stub_chain(:rent_houses, :find).with('1').and_return(@rent_house)
+      get :edit, user_id: 1, id: 1
 
+      assigns[:rent_house].should eq @rent_house
+      response.should be_success
+    end
   end
 
   describe 'PUT update' do
+    it 'should save successful and redirect to show' do
+      current_user.stub_chain(:rent_houses, :find).with('1').and_return(@rent_house)
+      @rent_house.should_receive(:update_attributes).with('price' => '1222', 'information' => 'air conditioner').and_return(true)
+      put :update, user_id: 1, id: 1, rent_house: { price: '1222', information: 'air conditioner' } 
 
+      response.should be_redirect
+    end
   end
 
   describe 'DELETE destroy' do
