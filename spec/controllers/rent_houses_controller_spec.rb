a3903eb0896e620dea61e8be5745b55184e8e0b5
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-25 23:33:07 +0800

    reorder and add new/destroy test

--- spec/controllers/rent_houses_controller_spec.rb
+++ spec/controllers/rent_houses_controller_spec.rb
@@ -5,7 +5,7 @@ describe RentHousesController do
   before do
     @user = User.new
     @rent_house = RentHouse.new
-    @rent_house.stub!(to_params: '1')
+    @rent_house.stub!(to_param: '1')
     controller.stub!(:authenticate_user!)
     controller.stub!(:current_user).and_return(@user)
   end
@@ -13,7 +13,24 @@ describe RentHousesController do
   let(:current_user) { @user }
 
   describe 'GET show' do
+    it 'should render successful' do
+      #RentHouse.should_receive(:find).with('1').and_return(@rent_house) 
+      get :show, user_id: 1, id: 1
+    end
+  end
 
+  describe 'GET new' do
+    it 'should render successful' do
+      RentHouse.should_receive(:new).and_return(@rent_house)
+      get :new, user_id: 1
+      assigns[:rent_house].should eq @rent_house
+      response.should be_success
+    end
+  end
+
+
+  describe 'POST create' do
+    
   end
 
   describe 'GET edit' do
@@ -24,17 +41,13 @@ describe RentHousesController do
 
   end
 
-  describe 'POST create' do
-
-  end
-
-  describe 'GET new' do
-
-  end
-
   describe 'DELETE destroy' do
-
+    it 'should delete successful' do
+      current_user.stub_chain(:rent_houses, :find).with('1').and_return(@rent_house)
+      @rent_house.should_receive(:destroy)
+      delete :destroy, user_id: 1, id: 1
+      response.should be_redirect
+    end 
   end
-
 
 end
