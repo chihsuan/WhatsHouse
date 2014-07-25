c8f2de2679ab605f9e84453295129611e3160f48
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-25 22:02:38 +0800

    create test mothod

--- spec/controllers/rent_houses_controller_spec.rb
+++ spec/controllers/rent_houses_controller_spec.rb
@@ -1,4 +1,40 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 describe RentHousesController do
+
+  before do
+    @user = User.new
+    @rent_house = RentHouse.new
+    @rent_house.stub!(to_params: '1')
+    controller.stub!(:authenticate_user!)
+    controller.stub!(:current_user).and_return(@user)
+  end
+
+  let(:current_user) { @user }
+
+  describe 'GET show' do
+
+  end
+
+  describe 'GET edit' do
+
+  end
+
+  describe 'PUT update' do
+
+  end
+
+  describe 'POST create' do
+
+  end
+
+  describe 'GET new' do
+
+  end
+
+  describe 'DELETE destroy' do
+
+  end
+
+
 end
