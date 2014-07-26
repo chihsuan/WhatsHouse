fb654a96f3e466dd7d7d594398dda93b7d5c0efb
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-26 20:44:49 +0800

    move maps to rent_houses and remvoe map controller

--- app/controllers/maps_controller.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class MapsController < ApplicationController
-
-  #show house data in map
-  def show
-    gon.real_price_list = RentHouse.initial.as_json()
-  end
-end
